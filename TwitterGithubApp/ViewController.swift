fdbf8edd5961404204e011e207fe8855ba573fd1
Simon <simon.delgd@gmail.com>
2025-01-01 19:00:36 +0100

    Initial Commit

---
+++ TwitterGithubApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterGithubApp
+//
+//  Created by Simon Delgado on 1/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
